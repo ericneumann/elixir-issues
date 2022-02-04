a084ac73cd7442805c1a7b528052291b2bb5d48f
Eric Neumann <ene1@hey.com>
2022-02-05 09:18:09 +1000

    refactor option parsing, add httpoison

--- lib/issues/cli.ex
+++ lib/issues/cli.ex
@@ -9,7 +9,9 @@ defmodule Issues.CLI do
   """
 
   def run(argv) do
-    parse_args(argv)
+    argv
+    |> parse_args
+    |> process
   end
 
   @doc """
@@ -22,18 +24,31 @@ defmodule Issues.CLI do
   help was given.
   """
   def parse_args(argv) do
-    parse = OptionParser.parse(argv, switches: [ help: :boolean],
-      aliases: [ h: :help])
-    case parse do
-      { [ help: true ], _, _ }
-        -> :help
-      { _, [ user, project, count ], _ }
-        -> { user, project, String.to_integer(count) }
-      { _, [ user, project ], _ }
-        -> { user, project, @default_count }
+    OptionParser.parse(argv, switches: [ help: :boolean], aliases: [ h: :help])
+      |> elem(1)
+      |> args_to_internal_representation()
+  end
 
-      _ -> :help
+  def args_to_internal_representation([user, project, count]) do
+    { user, project, String.to_integer(count) }
+  end
 
-    end
+  def args_to_internal_representation([user, project]) do
+    { user, project, @default_count }
+  end
+
+  def args_to_internal_representation(_) do
+    :help
+  end
+
+  def process(:help) do
+    IO.puts """
+    usage: issues <user> <project> [ count | #{@default_count} ]
+    """
+    System.halt(0)
+  end
+
+  def process( {user, project, _count}) do
+    Issues.GithubIssues.fetch(user, project)
   end
 end
